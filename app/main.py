c4689d536b2239f68b4e2b0494a10f4376651994
strauuch <popokoa@gmail.com>
2026-04-20 16:57:30 +0200

    feat: add api version and router for accounts

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from app.core.config import settings
 
-app = FastAPI(title="Online Cinema")
+from app.routes import accounts_router
 
+app = FastAPI(title="Online Cinema",
+    description="Description of project")
+
+api_version_prefix = "/api/v1"
+
+app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
 
 @app.get("/")
 def read_root():

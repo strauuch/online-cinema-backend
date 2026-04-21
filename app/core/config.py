a81731827a9d24e087b3f6ecb979e8cafe059dfc
strauuch <popokoa@gmail.com>
2026-04-21 16:47:36 +0200

    fix: update settings and add

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,59 @@
 import os
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    # Path
+    BASE_DIR: Path = Path(__file__).parent.parent
+    PATH_TO_DB: str = str(BASE_DIR / "database" / "source" / "theater.db")
+    PATH_TO_MOVIES_CSV: str = str(
+        BASE_DIR / "database" / "seed_data" / "imdb_movies.csv"
+    )
+
+    # Email
+    PATH_TO_EMAIL_TEMPLATES_DIR: str = str(BASE_DIR / "notifications" / "templates")
+    ACTIVATION_EMAIL_TEMPLATE_NAME: str = "activation_request.html"
+    ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME: str = "activation_complete.html"
+    PASSWORD_RESET_TEMPLATE_NAME: str = "password_reset_request.html"
+    PASSWORD_RESET_COMPLETE_TEMPLATE_NAME: str = "password_reset_complete.html"
+
+    EMAIL_HOST: str = "host"
+    EMAIL_PORT: int = 25
+    EMAIL_HOST_USER: str = "testuser"
+    EMAIL_HOST_PASSWORD: str = "test_password"
+    EMAIL_USE_TLS: bool = False
+    MAILHOG_API_PORT: int = 8025
+
+    # S3_Storage
+    S3_STORAGE_HOST: str = "minio-cinema"
+    S3_STORAGE_PORT: int = 9000
+    S3_STORAGE_ACCESS_KEY: str = "minioadmin"
+    S3_STORAGE_SECRET_KEY: str = "some_password"
+    S3_BUCKET_NAME: str = "cinema-storage"
+
+    # Database
     POSTGRES_DB: str
     POSTGRES_DB_PORT: int
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_HOST: str
 
+    # JWT
+    SECRET_KEY_ACCESS: str = "your_super_secret_access_key_here"
+    SECRET_KEY_REFRESH: str = "your_super_secret_refresh_key_here"
+    JWT_SIGNING_ALGORITHM: str = "HS256"
+    LOGIN_TIME_DAYS: int = 7
+
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+    @property
+    def S3_STORAGE_ENDPOINT(self) -> str:
+        return f"http://{self.S3_STORAGE_HOST}:{self.S3_STORAGE_PORT}"
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_DB_PORT}/{self.POSTGRES_DB}"
 
 
 settings = Settings()

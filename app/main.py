abdd07b73977dba0eed14bd57353ff1409345b33
strauuch <popokoa@gmail.com>
2026-04-21 16:48:29 +0200

    feat: create accounts router and

--- app/main.py
+++ app/main.py
@@ -3,12 +3,14 @@ from app.core.config import settings
 
 from app.routes import accounts_router
 
-app = FastAPI(title="Online Cinema",
-    description="Description of project")
+app = FastAPI(title="Online Cinema", description="Description of project")
 
 api_version_prefix = "/api/v1"
 
-app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
+app.include_router(
+    accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"]
+)
+
 
 @app.get("/")
 def read_root():

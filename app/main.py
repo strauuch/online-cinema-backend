3fbe1d7b0ad519dc081edddfc37788c3ef24c5fb
strauuch <popokoa@gmail.com>
2026-04-27 23:59:25 +0200

    feat: integrate movies router into main application

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from core.config import settings
 
-from routes import accounts_router
+from routes import accounts_router, movies_router
 
 app = FastAPI(title="Online Cinema", description="Description of project")
 
@@ -9,6 +9,9 @@ api_version_prefix = "/api/v1"
 
 app.include_router(
     accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"]
+)
+app.include_router(
+    movies_router, prefix=f"{api_version_prefix}/movies", tags=["movies"]
 )
 
 

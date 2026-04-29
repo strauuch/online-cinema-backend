9dbbd15c49e7562d4ead778d73aeae1a0964466e
strauuch <popokoa@gmail.com>
2026-04-29 22:13:42 +0200

    code: black movies_validators

--- app/database/validators/movies_validators.py
+++ app/database/validators/movies_validators.py
@@ -10,15 +10,18 @@ def validate_movie_year(year: int) -> int:
         raise ValueError(f"Year cannot be greater than {current_year + 5}.")
     return year
 
+
 def validate_imdb_rating(rating: float) -> float:
     if not 0 <= rating <= 10:
         raise ValueError("IMDb rating must be between 0 and 10.")
     return rating
 
+
 def validate_movie_price(price: decimal.Decimal) -> decimal.Decimal:
     if price < 0:
         raise ValueError("Price cannot be negative.")
     return price
+
 
 def validate_movie_duration(time: int) -> int:
     if time <= 0:

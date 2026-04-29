2ac4621fe7c4cc338edeea5107e12ec15cace30f
strauuch <popokoa@gmail.com>
2026-04-30 01:26:50 +0200

    chore: migrations

--- app/database/migrations/versions/b370013e9037_add_rating_stats_columns_to_movies.py
+++ app/database/migrations/versions/b370013e9037_add_rating_stats_columns_to_movies.py
@@ -5,15 +5,15 @@ Revises: 5b1c1b049dc9
 Create Date: 2026-04-29 22:27:59.503944
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
-revision: str = 'b370013e9037'
-down_revision: Union[str, Sequence[str], None] = '5b1c1b049dc9'
+revision: str = "b370013e9037"
+down_revision: Union[str, Sequence[str], None] = "5b1c1b049dc9"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 

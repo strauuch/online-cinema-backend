2d3a9d633d8b4493609330799c056ce3e5fbaf2f
strauuch <popokoa@gmail.com>
2026-04-29 02:00:25 +0200

    feat: define notification and comment schemas

--- app/schemas/movies.py
+++ app/schemas/movies.py
@@ -2,7 +2,9 @@ from datetime import datetime
 from decimal import Decimal
 from typing import List, Optional
 from uuid import UUID
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict, computed_field
+
+from database.models.enums import NotificationType
 
 
 class GenreReadSchema(BaseModel):
@@ -85,3 +87,41 @@ class RatingReadSchema(RatingCreateSchema):
 
 class VoteCreateSchema(BaseModel):
     is_like: bool  # True = Like, False = Dislike
+
+
+class NotificationReadSchema(BaseModel):
+    id: int
+    notification_type: NotificationType
+    content: str
+    link_to_id: Optional[str]
+    is_read: bool
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class CommentCreateSchema(BaseModel):
+    text: str = Field(..., min_length=1, max_length=500)
+    parent_id: Optional[int] = None
+
+
+class CommentAuthorSchema(BaseModel):
+    email: str
+    model_config = ConfigDict(from_attributes=True)
+
+
+class CommentReadSchema(BaseModel):
+    id: int
+    user_id: int
+    text: str
+    parent_id: Optional[int] = None
+    created_at: datetime
+
+    user: CommentAuthorSchema = Field(exclude=True)
+
+    @computed_field
+    @property
+    def email(self) -> str:
+        return self.user.email
+
+    model_config = ConfigDict(from_attributes=True)

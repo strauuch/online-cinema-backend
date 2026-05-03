8485448acd8ff65f36af509a581256902e31b387
strauuch <popokoa@gmail.com>
2026-05-03 20:59:09 +0200

    chore: generate alembic migrations for movie model updates

--- app/database/migrations/versions/1d392759fc0e_update_movie_models.py
+++ app/database/migrations/versions/1d392759fc0e_update_movie_models.py
@@ -5,15 +5,15 @@ Revises: 381028584b95
 Create Date: 2026-05-03 14:19:23.057365
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
-revision: str = '1d392759fc0e'
-down_revision: Union[str, Sequence[str], None] = '381028584b95'
+revision: str = "1d392759fc0e"
+down_revision: Union[str, Sequence[str], None] = "381028584b95"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 

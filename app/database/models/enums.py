f1d453522f5750193fdf7000858dd8df7ac51d5d
strauuch <popokoa@gmail.com>
2026-05-07 00:10:08 +0200

    feat: create new enums for orders and payments

--- app/database/models/enums.py
+++ app/database/models/enums.py
@@ -16,3 +16,14 @@ class NotificationType(enum.Enum):
     COMMENT_LIKE = "comment_like"
     COMMENT_REPLY = "comment_reply"
     SYSTEM = "system"
+
+
+class OrderStatusEnum(str, enum.Enum):
+    PENDING = "pending"
+    PAID = "paid"
+    CANCELED = "canceled"
+
+class PaymentStatusEnum(str, enum.Enum):
+    SUCCESSFUL = "successful"
+    CANCELED = "canceled"
+    REFUNDED = "refunded"

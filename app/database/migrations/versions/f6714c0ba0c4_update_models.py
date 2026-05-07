b36a089e32770477f076b547fb1fbc9e80f31afd
strauuch <popokoa@gmail.com>
2026-05-07 17:01:18 +0200

    chore: black migrations file

--- app/database/migrations/versions/f6714c0ba0c4_update_models.py
+++ app/database/migrations/versions/f6714c0ba0c4_update_models.py
@@ -5,15 +5,15 @@ Revises: 3719a8e7734a
 Create Date: 2026-05-07 14:50:57.179430
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
-revision: str = 'f6714c0ba0c4'
-down_revision: Union[str, Sequence[str], None] = '3719a8e7734a'
+revision: str = "f6714c0ba0c4"
+down_revision: Union[str, Sequence[str], None] = "3719a8e7734a"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
